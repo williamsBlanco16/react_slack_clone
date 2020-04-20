f32e50d342c0c9ab78623c81857cf2a47274128e
Williams Blanco <williamsblanco16@gmail.com>
2020-04-20 16:43:38 -0400

    add style to background of app

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -19,7 +19,7 @@ class Register extends Component {
 
   render() {
     return (
-      <Grid textAlign='center' verticalAlign='middle'>
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
         <Grid.Column style={{maxWidth:450}}>
           <Header as='h2' icon color='orange' textAlign='center'>
             <Icon name='puzzle piece' color='orange'/>
