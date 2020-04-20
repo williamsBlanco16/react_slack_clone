df1c01c54e551762465a760c0b86f806020092c6
Williams Blanco <williamsblanco16@gmail.com>
2020-04-20 14:44:23 -0400

    add firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAFccpBteMMR-t_NDD4T3a-5hcrAIPhxfI",
+  authDomain: "react-slack-clone-96c8b.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-96c8b.firebaseio.com",
+  projectId: "react-slack-clone-96c8b",
+  storageBucket: "react-slack-clone-96c8b.appspot.com",
+  messagingSenderId: "1000586208411",
+  appId: "1:1000586208411:web:21d0e4c93c1d073ac7923e",
+  measurementId: "G-20GSH3FY4V"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App';
+import Register from './components/Auth/Register'
+import Login from './components/Auth/Login'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+const Root = () => (
+  <Router> 
+    <Switch>
+      <Route exact path="/" component = {App}/>
+      <Route path="/login" component = {Login}/>
+      <Route path="/register" component = {Register}/>
+
+    </Switch> 
+     
+  </Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
