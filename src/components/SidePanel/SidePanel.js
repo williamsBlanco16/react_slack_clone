537dd8e7629a1f649c1c11ada4ffa9ef76ade793
Williams Blanco <williamsblanco16@gmail.com>
2020-04-23 02:00:10 -0400

    add modal channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {Menu} from 'semantic-ui-react'
 import UserPanel from './UserPanel'
+import Channels from './Channels'
 
 class SidePanel extends Component {
   render() {
@@ -13,6 +14,7 @@ class SidePanel extends Component {
         style={{background:'#4c3c4c', fontSize:'1.2rem'}}
       >
         <UserPanel/>
+        <Channels/>
       </Menu>
     )
   }
